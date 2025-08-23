f5d2d3abe48119135471a9c83bd48debd5327ce6
chumair91 <umairkhalid2112@gmail.com>
2025-08-23 17:02:13 +0500

    add

--- Tut9 ( Ejs practice set)/Routes/hostRouter.js
+++ Tut9 ( Ejs practice set)/Routes/hostRouter.js
@@ -6,7 +6,8 @@ const rootDir = require('../utils/utilPath');
 hostRouter.get('/host/add-home', (req, res, next) => {
   // res.sendFile(path.join(__dirname, '../', 'view', 'addHome.html'));
   // res.sendFile(path.join(rootDir, 'views', 'addHome.html'));
-  res.render('addHome',{content:'Add your Home'});
+  res.render('addHome');
+  res.render('addHome',{content:'Add your Home',title: 'Add Home'});
 });
 const registeredHomes = [];
 
@@ -14,7 +15,8 @@ hostRouter.post('/host/add-home', (req, res, next) => {
   registeredHomes.push({ houseName: req.body.houseName });
 
   // res.sendFile(path.join(rootDir, 'views', 'homeAdded.html'));
-  res.render('homeAdded');
+
+  res.render('homeAdded',{content: 'Home Added Successfully',title:"Success"});
 });
 
 exports.hostRouter = hostRouter;
